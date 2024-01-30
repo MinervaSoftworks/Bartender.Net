7a9dbfe021d51137b09c85553fd86624ab3601b0
Raven Crowe <ravencrowe01@gmail.com>
2024-01-29 23:57:54 -0500

    Small fix

--- TornApi.Net.Models/Faction/CrimeExperience.cs
+++ TornApi.Net.Models/Faction/CrimeExperience.cs
@@ -1,12 +1,9 @@
 ﻿using Newtonsoft.Json;
-using TornApi.Net.Models.Faction;
+
+namespace TornApi.Net.Models.Faction; 
 
 public class CrimeExperience {
-
-
     public int CrimeEpxerienceID { get; set; }
-
-    public Basic Faction { get; set; }
 
     [JsonProperty ("crimeexp")]
     public IEnumerable<int> Members { get; set; }
