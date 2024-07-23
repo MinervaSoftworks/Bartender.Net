acf17bfd5d279f9f798c6858432bdfe17bcff9e2
Raven Crowe <ravencrowe01@gmail.com>
2024-07-22 21:33:47 -0400

    Added log models

---
+++ Bartender.Net.User/Log/LogEntry.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+
+namespace Bartender.Net.User.Log;
+
+public class LogEntry {
+    [JsonIgnore]
+    public required int ID { get; set; }
+
+    [JsonProperty ("category")]
+    public required string Category { get; set; }
+
+    [JsonProperty ("data")]
+    public required IDictionary<string, int> Data { get; set; }
+
+    [JsonProperty ("log")]
+    public required string LogType { get; set; }
+
+    [JsonProperty ("params")]
+    public required IDictionary<string, string> Params { get; set; }
+
+    [JsonProperty ("timestamp")]
+    public required long Timestamp { get; set; }
+
+    [JsonProperty ("title")]
+    public required string Title { get; set; }
+}
---
+++ Bartender.Net.User/Log/UserLog.cs
@@ -0,0 +1,8 @@
+﻿using Newtonsoft.Json;
+
+namespace Bartender.Net.User.Log;
+
+public class UserLog {
+    [JsonProperty("log")]
+    public required IDictionary<int, LogEntry> Entries { get; set; }
+}
